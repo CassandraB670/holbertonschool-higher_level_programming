3a8f6b41b5cb8b0da43e2d1743f47fb9af177d25
CassandraB670 <cassandra.boudier@gmail.com>
2023-09-28 08:39:17 +0000

    change line 7 for print .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for number1 in range(0, 10):
     for number2 in range(number1 + 1, 10):
         if number1 == 8 and number2 == 9:
             separate = "\n"
-        print(f"{number1}{number2}", end=separate)
+        print("{}{}".format(number1, number2), end=separate)
