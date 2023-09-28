3f05b3d68f56ce67b3e5c06327b326f1355768f2
CassandraB670 <cassandra.boudier@gmail.com>
2023-09-28 08:35:49 +0000

    add file with script for prints all possible different combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+separate = ", "
+for number1 in range(0, 10):
+    for number2 in range(number1 + 1, 10):
+        if number1 == 8 and number2 == 9:
+            separate = "\n"
+        print(f"{number1}{number2}", end=separate)
