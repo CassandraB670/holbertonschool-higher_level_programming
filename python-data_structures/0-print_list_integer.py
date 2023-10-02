11042233e2b212f273d9a9b5dbb4c857f5922b11
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-02 21:42:32 +0000

    delete line 4 and change line 5

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for index in my_list:
-        integer = int(index)
-        print("{}".format(integer))
+        print("{:d}".format(index))
