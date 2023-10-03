d68f28dd377a5746bf6c70a38067ca285e8106cc
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-03 12:59:45 +0000

    change all the code

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        completeTupleA = tuple_a + (0,) * (2 - len(tuple_a))
-        completeTupleB = tuple_b + (0,) * (2 - len(tuple_b))
-    elif len(tuple_a) > 2 or len(tuple_b) > 2:
-        completeTupleA = tuple_a[:2]
-        completeTupleB = tuple_b[:2]
-    else:
-        completeTupleA = tuple_a
-        completeTupleB = tuple_b
-    res = []
-    for index in range(0, len(tuple_a)):
-        res.append(completeTupleA[index]+completeTupleB[index])
-    res = tuple(res)
-    return res
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
