70c89a50d3ad2761f04280cc6d74ae013cf72a96
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-03 12:54:13 +0000

    add file with fct that adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2 or len(tuple_b) < 2:
+        completeTupleA = tuple_a + (0,) * (2 - len(tuple_a))
+        completeTupleB = tuple_b + (0,) * (2 - len(tuple_b))
+    elif len(tuple_a) > 2 or len(tuple_b) > 2:
+        completeTupleA = tuple_a[:2]
+        completeTupleB = tuple_b[:2]
+    else:
+        completeTupleA = tuple_a
+        completeTupleB = tuple_b
+    res=[]
+    for index in range(0, len(tuple_a)):
+        res.append(completeTupleA[index]+completeTupleB[index])
+    res=tuple(res)
+    return res
