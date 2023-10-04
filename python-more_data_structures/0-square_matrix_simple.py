f6ac48858c1fd8929ae484b1aa5d5e42a5e80084
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-04 08:31:10 +0000

    add line at the end of the code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     result = []
 
     for row in matrix:
-        square =[]
+        square = []
         for index in row:
             square.append(index ** 2)
         result.append(square)
