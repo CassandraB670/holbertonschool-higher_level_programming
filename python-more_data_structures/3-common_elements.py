27a07595046ef3a2bc3844074bb472ee2a0a6228
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-04 09:45:59 +0000

    change all the code

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for index in set_1:
-        for index2 in set_2:
-            if index2 == index:
-                return index
+    result = [i for i in set_1 if i in set_2]
+    return result
