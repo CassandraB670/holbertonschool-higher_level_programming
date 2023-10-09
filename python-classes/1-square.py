02abc924616e68827d0860270b143f74fbb377e2
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-09 09:02:20 +0000

    update README.md and add file with class square that defines a square with his size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """Empty class Square that defines a square with size."""
+    def __init__(self, size):
+        """square size private instance attribute"""
+        self.__size = size
