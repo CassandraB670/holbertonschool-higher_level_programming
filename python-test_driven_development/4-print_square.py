a74e84fb4c3e56875a703491e9fecf006df8ae15
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-12 12:09:52 +0000

    delete indentation line 22

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -19,6 +19,6 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    
+
     for index in range(size):
         print("#" * size)
