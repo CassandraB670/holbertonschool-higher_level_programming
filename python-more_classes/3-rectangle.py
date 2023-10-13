1e01f5600317824dd62ffd9b14b07c8bd68d1d81
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-13 11:51:42 +0000

    delete whitespace line 74

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -71,7 +71,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-        
+
     def __str__(self):
         """Print a rectangle using #
 
