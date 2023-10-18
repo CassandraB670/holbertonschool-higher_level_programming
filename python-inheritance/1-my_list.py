22feeb2084dcb84a527b850a50ad3cdb47055fc0
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-18 22:07:56 +0000

    class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""write a class that inherits from list"""
+
+class MyList(list):
+    """class that inherits frome list"""
+
+    def print_sorted(self):
+        """print list"""
+
+        if issubclass(MyList, list):
+            print(sorted(self))
