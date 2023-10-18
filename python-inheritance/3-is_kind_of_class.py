13c1fb1ce6bb00bf398ac8fd2aaac3d385d78dc4
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-18 20:39:30 +0000

    change pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,13 +6,13 @@ class"""
 
 def is_kind_of_class(obj, a_class):
     """write a function that returns True if the object is an instance of,
-    or if the object is an instance of a class that inherited from, the specified
-    class
+    or if the object is an instance of a class that inherited from, the
+    specified class
 
     Args:
         obj (_type_): _description_
         a_class (_type_): _description_
-    
+
     Return : True of False
     """
 
