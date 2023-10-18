4656f28030acac5e91f83f63eaf32979419d12f5
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-18 22:03:02 +0000

    class Square with inherits

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """instantiation with size
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """area of the square"""
+        return self.__size ** 2
+    
+    def __str__(self):
+        """print and return the square description"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
