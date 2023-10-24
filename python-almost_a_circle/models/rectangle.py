e7eb70183c58c89030f1c99b91665692b03b7824
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-24 10:33:36 +0000

    change line 116, 92, 68, 43 fordefine if the attributes is an intger

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -40,7 +40,7 @@ class Rectangle(Base):
             ValueError: if value is < 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -65,7 +65,7 @@ class Rectangle(Base):
             ValueError: if value is < 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -89,7 +89,7 @@ class Rectangle(Base):
             ValueError: if value is < 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -113,7 +113,7 @@ class Rectangle(Base):
             ValueError: if value is < 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
