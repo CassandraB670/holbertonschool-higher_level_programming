4c23c85fc4cbe2eedf262110d558399ff23c23db
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-27 12:01:19 +0000

    add def from_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,6 +31,17 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """return the list of the JSON string representation
+
+        Args:
+            json_string : string representing a list of dictionaries
+        """
+        if json_string is None or json_string == "[]":
+            return "[]"
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """write the JSON string representation of list_objs to a file
