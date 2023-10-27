11b8125aa135c709cb77304c9b2c89c8929b1141
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-27 09:28:43 +0000

    add def to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Write the first class Base"""
 
+import json
+
+
 
 class Base:
     """base of all other classes in this project"""
@@ -17,3 +20,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries : list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
