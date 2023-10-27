27c77c0eb989fd292a824c55ed6f0707c23fd441
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-27 11:54:57 +0000

    add def save_to_file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,7 +4,6 @@
 import json
 
 
-
 class Base:
     """base of all other classes in this project"""
     __nb_objects = 0
@@ -31,3 +30,20 @@ class Base:
         if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs : list of instances who inherits of Base
+        """
+        if list_objs is None:
+            list_objs = []
+
+        filename = f"{cls.__name__}.json"
+        data = [obj.to_dictionary() for obj in list_objs]
+        json_str = Base.to_json_string(data)
+
+        with open(filename, 'w') as file:
+            file.write(json_str)
