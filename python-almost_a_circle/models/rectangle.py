ffa5fef8b1e31f6d210b7c2033142ac2d6ed06e7
CassandraB670 <cassandra.boudier@gmail.com>
2023-10-27 08:16:33 +0000

    add **kwargs in def update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -136,15 +136,13 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """assigns an argument to each attribtue"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            attributs = [self.id, self.width, self.height, self.x, self.y]
+            for index, arg in enumerate(args):
+                if index < len(attributs):
+                    setattr(self, attributs[index], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
