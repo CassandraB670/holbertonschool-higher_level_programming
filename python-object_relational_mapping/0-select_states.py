bf0150a07cdb568ca8823192258a1f41376c15f6
CassandraB670 <cassandra.boudier@gmail.com>
2023-11-29 19:30:27 +0100

    change authorization

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that lists all states from the databse"""
+""" script that lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
@@ -26,3 +27,4 @@ if __name__ == "__main__":
 
     cursor.close()
     database.close()
+    
