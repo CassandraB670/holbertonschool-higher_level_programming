9827d000bb860513193a5ede43ca5a5e4fbb573d
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-01 22:30:57 +0100

    script that changes the name of a State object from the database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script that lists all State objects from the database"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        mysql_username,
+        mysql_password,
+        db_name
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    qr = session.query(
+        State).filter(State.id == 2).update({State.name: 'New Mexico'})
+
+    session.commit()
+
+    session.close()
