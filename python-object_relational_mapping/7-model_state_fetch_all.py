bcaab05405d096bf9941263b9c30353387069da8
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-01 11:36:09 +0100

    update README and add file with script that lists all State objects from the database

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script that lists all State objects from the database"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        db
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id).all()
+
+    session.close()
+
+    for state in query:
+        print("{}: {}".format(state.id, state.name))
