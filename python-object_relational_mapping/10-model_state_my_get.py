8fc6af3d42b060a6523ad44396d276e3c54e62cb
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-01 15:03:16 +0100

    add .first at the end of the line 26

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    qr = session.query(State).filter(State.name == state_name_to_search)
+    qr = session.query(State).filter(State.name == state_name_to_search).first()
 
     session.close()
 
